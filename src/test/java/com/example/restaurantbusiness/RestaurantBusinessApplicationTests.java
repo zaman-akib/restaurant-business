94cbee6f7b572216755713291808b8ffc014faf4
Akib Uz Zaman <akibuz@synopsys.com>
2024-02-15 15:12:18 +0600

    initialize application

---
+++ src/test/java/com/example/restaurantbusiness/RestaurantBusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restaurantbusiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantBusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
