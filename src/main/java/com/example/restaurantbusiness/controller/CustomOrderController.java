011f6b2f3948c09873d76033139fa2ca42feab34
Akib Uz Zaman <akibuz@synopsys.com>
2024-02-16 13:01:58 +0600

    add method getTotalSaleAmountForCurrentDay in CustomerOrderService and expose API in controller
    remove save method which as created for testing purpose

--- src/main/java/com/example/restaurantbusiness/controller/CustomOrderController.java
+++ src/main/java/com/example/restaurantbusiness/controller/CustomOrderController.java
@@ -2,13 +2,15 @@ package com.example.restaurantbusiness.controller;
 
 import com.example.restaurantbusiness.entity.CustomerOrder;
 import com.example.restaurantbusiness.service.CustomerOrderService;
+import java.math.BigDecimal;
 import java.util.List;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/orders")
+@RequestMapping("/api/customer-orders")
 public class CustomOrderController {
     private final CustomerOrderService customerOrderService;
 
@@ -17,7 +19,14 @@ public class CustomOrderController {
     }
 
     @GetMapping("/current-day")
-    public List<CustomerOrder> getOrdersForCurrentDay() {
-        return customerOrderService.getOrdersForCurrentDay();
+    public ResponseEntity<List<CustomerOrder>> getOrdersForCurrentDay() {
+        List<CustomerOrder> currentDayOrders = customerOrderService.getOrdersForCurrentDay();
+        return ResponseEntity.ok(currentDayOrders);
+    }
+
+    @GetMapping("/total-sale-amount/current-day")
+    public ResponseEntity<BigDecimal> getTotalSaleAmountForCurrentDay() {
+        BigDecimal totalSaleAmount = customerOrderService.getTotalSaleAmountForCurrentDay();
+        return ResponseEntity.ok(totalSaleAmount);
     }
 }
--- src/main/java/com/example/restaurantbusiness/service/CustomerOrderService.java
+++ src/main/java/com/example/restaurantbusiness/service/CustomerOrderService.java
@@ -2,6 +2,7 @@ package com.example.restaurantbusiness.service;
 
 import com.example.restaurantbusiness.entity.CustomerOrder;
 import com.example.restaurantbusiness.repository.CustomerOrderRepository;
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -15,13 +16,17 @@ public class CustomerOrderService {
         this.orderRepository = orderRepository;
     }
 
-    public CustomerOrder save(CustomerOrder customerOrder) {
-        return orderRepository.save(customerOrder);
-    }
-
     public List<CustomerOrder> getOrdersForCurrentDay() {
         LocalDateTime startOfDay = LocalDate.now().atStartOfDay();
         LocalDateTime endOfDay = startOfDay.plusHours(23).plusMinutes(59).plusSeconds(59);
         return orderRepository.findByOrderDateTimeBetween(startOfDay, endOfDay);
+    }
+
+    public BigDecimal getTotalSaleAmountForCurrentDay() {
+        List<CustomerOrder> currentDayOrders = getOrdersForCurrentDay();
+
+        return currentDayOrders.stream()
+            .map(CustomerOrder::getPrice)
+            .reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 }
