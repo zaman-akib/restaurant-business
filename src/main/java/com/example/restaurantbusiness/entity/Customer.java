42f3c9acea1a12b19bf2fc634a68a27b3c7f9a1c
Akib Uz Zaman <akibuz@synopsys.com>
2024-02-16 13:00:37 +0600

    add customer entity and create many to one relation with CustomerOrder entity

---
+++ src/main/java/com/example/restaurantbusiness/entity/Customer.java
@@ -0,0 +1,23 @@
+package com.example.restaurantbusiness.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "customer")
+@Getter
+@Setter
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String phone;
+}
--- src/main/java/com/example/restaurantbusiness/entity/CustomerOrder.java
+++ src/main/java/com/example/restaurantbusiness/entity/CustomerOrder.java
@@ -4,9 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
 
 @Entity
@@ -18,5 +22,13 @@ public class CustomerOrder {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
+
+    @NonNull
+    private BigDecimal price;
+
+    @NonNull
     private LocalDateTime orderDateTime;
 }
